fb7975b9e8e088d3e570ce18e36bdebfc77b3d11
Alfiansyah <achmadalfiansyah069@gmail.com>
2022-04-01 14:03:29 +0700

    add database encryption and obfuscation

--- core/src/main/java/com/achmadalfiansyah/moviecatalog/core/di/DatabaseModule.kt
+++ core/src/main/java/com/achmadalfiansyah/moviecatalog/core/di/DatabaseModule.kt
@@ -10,6 +10,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -19,11 +21,16 @@ class DatabaseModule {
     @Singleton
     @Provides
     fun provideAppDatabase(@ApplicationContext context: Context): ShowDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("alfiansyah".toCharArray())
+        val factory = SupportFactory(passphrase)
         return Room.databaseBuilder(
             context,
             ShowDatabase::class.java,
             "show.db"
-        ).fallbackToDestructiveMigration().build()
+        )
+            .fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 
     @Provides
