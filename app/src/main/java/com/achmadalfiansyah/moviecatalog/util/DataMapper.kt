a0102a20802b549172b957a20a1342f54fe498e6
Alfiansyah <achmadalfiansyah069@gmail.com>
2022-04-01 15:46:50 +0700

    update dependencies and remove redundant constructs

--- app/src/main/java/com/achmadalfiansyah/moviecatalog/util/DataMapper.kt
+++ app/src/main/java/com/achmadalfiansyah/moviecatalog/util/DataMapper.kt
@@ -1,14 +1,5 @@
 package com.achmadalfiansyah.moviecatalog.util
 
-import com.achmadalfiansyah.moviecatalog.core.data.source.local.entity.MovieEntity
-import com.achmadalfiansyah.moviecatalog.core.data.source.local.entity.TvShowEntity
-import com.achmadalfiansyah.moviecatalog.core.data.source.remote.response.MovieDetailResponse
-import com.achmadalfiansyah.moviecatalog.core.data.source.remote.response.MovieResponse
-import com.achmadalfiansyah.moviecatalog.core.data.source.remote.response.TvShowDetailResponse
-import com.achmadalfiansyah.moviecatalog.core.data.source.remote.response.TvShowResponse
-import com.achmadalfiansyah.moviecatalog.core.domain.model.Movie
-import com.achmadalfiansyah.moviecatalog.core.domain.model.TvShow
-
 object DataMapper {
     const val MOVIE = "Movie"
     const val TVSHOW = "Tv Show"
