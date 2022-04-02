80844ec7c1401d66354b558932477c83aa85eba0
Alfiansyah <achmadalfiansyah069@gmail.com>
2022-04-02 11:44:33 +0700

    add certificatePinner

--- core/src/main/java/com/achmadalfiansyah/moviecatalog/core/data/source/remote/api/ApiBuilder.kt
+++ core/src/main/java/com/achmadalfiansyah/moviecatalog/core/data/source/remote/api/ApiBuilder.kt
@@ -2,10 +2,12 @@ package com.achmadalfiansyah.moviecatalog.core.data.source.remote.api
 
 
 import com.achmadalfiansyah.moviecatalog.core.BuildConfig
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiBuilder {
     private var retrofit: Retrofit = buildRetrofit(httpClient)
@@ -13,22 +15,21 @@ object ApiBuilder {
 
     private val httpClient: OkHttpClient
         get() {
+            val hostname = "api.themoviedb.org"
+            val certificatePinner = CertificatePinner.Builder()
+                .add(hostname, "sha256/oD/WAoRPvbez1Y2dfYfuo4yujAcYHXdv1Ivb2v2MOKk=")
+                .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+                .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+                .build()
             val loggingInterceptor = HttpLoggingInterceptor()
             loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
 
-            var builder: OkHttpClient.Builder = OkHttpClient.Builder()
-                .addInterceptor {
-                    val url = it.request()
-                        .url
-                        .newBuilder()
-                        .build()
-                    val request = it.request()
-                        .newBuilder()
-                        .url(url)
-                        .build()
-
-                    it.proceed(request)
-                }
+            var builder: OkHttpClient.Builder = OkHttpClient.Builder().apply {
+                addInterceptor(loggingInterceptor)
+                certificatePinner(certificatePinner)
+                connectTimeout(5, TimeUnit.SECONDS)
+                readTimeout(5, TimeUnit.SECONDS)
+            }
             if (BuildConfig.DEBUG) {
                 builder = builder.addInterceptor(loggingInterceptor)
             }
